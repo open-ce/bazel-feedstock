7fca779d927e689527280f8240a84b32ab49f705
Nishidha <npanpa23@in.ibm.com>
2022-07-25 13:57:17 +0530

    Updated to bazel 5.1.1 (#25)
    * Updated to bazel 5.1.1
    
    * Use RH toolchain for P10 builds
    
    * Added s390x support for Bazel 3.7.2 (#20)
    
    - Included md5digest state byte order fix.
    - Increase Java heap size to fix OutOfMemory issue.
    
    * Fixed bazel build on s390x
    
    Co-authored-by: Pradipta Ghosh <37842314+pradghos@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,6 +20,15 @@ set -v -x
 #export BAZEL_BUILD_OPTS="--logging=6 --subcommands --verbose_failures"
 export EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk"
 
+if [[ $ppc_arch == "p10" ]]
+then
+    if [[ -z "${GCC_11_HOME}" ]];
+    then
+        echo "Please set GCC_11_HOME to the install path of gcc-toolset-11"
+        exit 1
+    fi
+fi
+
 #Linux - set flags for statically linking libstdc++
 # xref: https://github.com/bazelbuild/bazel/blob/0.12.0/tools/cpp/unix_cc_configure.bzl#L257-L258
 # xref: https://github.com/bazelbuild/bazel/blob/0.12.0/tools/cpp/lib_cc_configure.bzl#L25-L39
@@ -40,6 +49,16 @@ if [[ ${target_platform} =~ .*ppc.* ]]; then
 
 elif [[ ${target_platform} =~ .*x86_64.* || ${target_platform} =~ .*linux-64.* ]]; then
   export JAVA_HOME=$BUILD_PREFIX
+  SYSROOT_DIR="${BUILD_PREFIX}"/x86_64-conda_cos6-linux-gnu/sysroot/usr/
+elif [[ ${target_platform} =~ .*s390x.* ]]; then
+  SYSROOT_DIR="${BUILD_PREFIX}"/s390x-conda_cos7-linux-gnu/sysroot/usr/
+  jvm_slug=$(compgen -G "${SYSROOT_DIR}/lib/jvm/java-11-openjdk-*")
+  export JAVA_HOME=${jvm_slug}
+
+  #Use the zip CDT
+  zip_slug="${SYSROOT_DIR}"/bin
+  export PATH=$PATH:${zip_slug}
+
 fi
 
 export PATH=$PATH:$JAVA_HOME/bin
